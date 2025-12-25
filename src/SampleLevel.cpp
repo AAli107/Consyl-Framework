71b4aab848bc37d3081589abef45ee38cbc5db7f
Ali107 <Alialawadhi2014bh@gmail.com>
2025-12-25 16:18:26 +0300

    buncha comments

--- src/SampleLevel.cpp
+++ src/SampleLevel.cpp
@@ -3,17 +3,20 @@
 
 #include "SampleLevel.h"
 
-void SampleLevel::start(GameLoop &gl)
+// This has bunch of test code, so clean them up if you want to use the SampleLevel or
+// you could make your own level by making a class inherit Level abstract class
+
+void SampleLevel::start(GameLoop &gl) // Runs when the level starts
 {
     gl.showStats = true;
 }
 
-void SampleLevel::update(GameLoop &gl)
+void SampleLevel::update(GameLoop &gl) // Runs every frame while the level is loaded
 {
     // if (gl.timeRunning() > 10) gl.stop();
 }
 
-void SampleLevel::render(GameLoop &gl, Gfx &gfx)
+void SampleLevel::render(GameLoop &gl, Gfx &gfx) // Runs after right after update() for graphics rendering
 {
     gfx.drawRect(Vec2(5, 5), Vec2(10, 10), '#', '.');
     gfx.drawCircle(32, 20, 10, '#', '-');
@@ -29,6 +32,6 @@ void SampleLevel::render(GameLoop &gl, Gfx &gfx)
     gfx.drawText(Vec2(30, 40), fpsOss.str());
 }
 
-void SampleLevel::end(GameLoop &gl)
+void SampleLevel::end(GameLoop &gl) // Runs when level ends
 {
 }
