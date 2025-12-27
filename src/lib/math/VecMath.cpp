147f386fccb85fd3101959743160d2db3d8f0386
Ali107 <Alialawadhi2014bh@gmail.com>
2025-12-27 14:33:17 +0300

    Added blank definitions for the new functions.

--- src/lib/math/VecMath.cpp
+++ src/lib/math/VecMath.cpp
@@ -15,7 +15,7 @@ Vec3 Cross(const Vec3 v1, const Vec3 v2) { return Vec3( v1.y * v2.z - v1.z * v2.
 bool isInPolygon(const Vec2& v, const Vec2 poly[], int polyLen)
 {
     if (polyLen < 3) return false;
-    
+
     int windingN = 0;
 
     for (int i = 0; i < polyLen; i++)
@@ -37,3 +37,42 @@ bool isInPolygon(const Vec2& v, const Vec2 poly[], int polyLen)
     return windingN != 0;
 }
 
+bool isInBox2D(const Vec2 &v, const Vec2 boxPos, const Vec2 dimension)
+{
+    return false;
+}
+
+bool isBox2DIntersecting(const Vec2 box0Pos, const Vec2 box0Dim, const Vec2 box1Pos, const Vec2 box1Dim)
+{
+    return false;
+}
+
+bool isInCircle(const Vec2 &v, const Vec2 circlePos, double circleRadius)
+{
+    return false;
+}
+
+bool isCircleIntersecting(const Vec2 circle0Pos, double circle0Radius, const Vec2 circle1Pos, double circle1Radius)
+{
+    return false;
+}
+
+bool isInBox3D(const Vec3 &v, const Vec3 boxPos, const Vec3 dimension)
+{
+    return false;
+}
+
+bool isBox3DIntersecting(const Vec3 box0Pos, const Vec3 box0Dim, const Vec3 box1Pos, const Vec3 box1Dim)
+{
+    return false;
+}
+
+bool isInSphere(const Vec3 &v, const Vec3 spherePos, double sphereRadius)
+{
+    return false;
+}
+
+bool isSphereIntersecting(const Vec3 sphere0Pos, double sphere0Radius, const Vec3 sphere1Pos, double sphere1Radius)
+{
+    return false;
+}
