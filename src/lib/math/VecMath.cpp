a98b4d04c82103a272c25f7698b75b1c8171abe5
Ali107 <Alialawadhi2014bh@gmail.com>
2025-12-27 19:24:45 +0300

    Updated VecMath functions' Vector parameters so that they would be constant references

--- src/lib/math/VecMath.cpp
+++ src/lib/math/VecMath.cpp
@@ -2,15 +2,19 @@
 
 #include <cmath>
 
-Vec2 project3D(Vec3 v) { return Vec2((v.x / v.z), (v.y / v.z)); }
+Vec2 project3D(const Vec3& v) { return Vec2((v.x / v.z), (v.y / v.z)); }
 
-Vec2 screenToNormalizedSpace(Vec2 v, int screenWidth, int screenHeight) { return Vec2( 2.0 * v.x / screenWidth - 1.0, 1.0 - 2.0 * v.y / screenHeight ); }
+Vec2 screenToNormalizedSpace(const Vec2& v, int screenWidth, int screenHeight) 
+{ return Vec2( 2.0 * v.x / screenWidth - 1.0, 1.0 - 2.0 * v.y / screenHeight ); }
 
-Vec2 normalizedToScreenSpace(Vec2 v, int screenWidth, int screenHeight) { return Vec2((v.x + 1.0) * 0.5 * screenWidth, (1.0 - (v.y + 1.0) * 0.5) * screenHeight); }
+Vec2 normalizedToScreenSpace(const Vec2& v, int screenWidth, int screenHeight) 
+{ return Vec2((v.x + 1.0) * 0.5 * screenWidth, (1.0 - (v.y + 1.0) * 0.5) * screenHeight); }
 
-Vec3 rotate2D(Vec3 v, double angle) { double c = std::cos(angle); double s = std::sin(angle); return Vec3(c * v.x - s * v.y, s * v.x + c * v.y, v.z); }
+Vec3 rotate2D(const Vec3& v, double angle)
+{ double c = std::cos(angle); double s = std::sin(angle); return Vec3(c * v.x - s * v.y, s * v.x + c * v.y, v.z); }
 
-Vec3 Cross(const Vec3 v1, const Vec3 v2) { return Vec3( v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x ); }
+Vec3 Cross(const Vec3& v1, const Vec3& v2)
+{ return Vec3( v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x ); }
 
 bool isInPolygon(const Vec2& point, const Vec2 poly[], int polyLen)
 {
@@ -37,14 +41,14 @@ bool isInPolygon(const Vec2& point, const Vec2 poly[], int polyLen)
     return windingN != 0;
 }
 
-bool isInBox2D(const Vec2 &point, const Vec2 boxPos, const Vec2 boxDim)
+bool isInBox2D(const Vec2& point, const Vec2& boxPos, const Vec2& boxDim)
 {
     if (boxDim.x < 0 || boxDim.y < 0) return false;
     Vec2 boxEnd = boxPos + boxDim;
     return point.x >= boxPos.x && point.x <= boxEnd.x && point.y >= boxPos.y && point.y <= boxEnd.y; 
 }
 
-bool isBox2DIntersecting(const Vec2 box0Pos, const Vec2 box0Dim, const Vec2 box1Pos, const Vec2 box1Dim)
+bool isBox2DIntersecting(const Vec2& box0Pos, const Vec2& box0Dim, const Vec2& box1Pos, const Vec2& box1Dim)
 {
     if (box0Dim.x < 0 || box0Dim.y < 0 || box1Dim.x < 0 || box1Dim.y < 0) return false;
     Vec2 box0End = box0Pos + box0Dim;
@@ -52,20 +56,20 @@ bool isBox2DIntersecting(const Vec2 box0Pos, const Vec2 box0Dim, const Vec2 box1
     return box0Pos.x <= box1End.x && box0End.x >= box1Pos.x && box0Pos.y <= box1End.y && box0End.y >= box1Pos.y; 
 }
 
-bool isInCircle(const Vec2 &point, const Vec2 circlePos, double circleRadius)
+bool isInCircle(const Vec2& point, const Vec2& circlePos, double circleRadius)
 { return circleRadius < 0 ? false : point.distance(circlePos) <= circleRadius; }
 
-bool isCircleIntersecting(const Vec2 circle0Pos, double circle0Radius, const Vec2 circle1Pos, double circle1Radius)
+bool isCircleIntersecting(const Vec2& circle0Pos, double circle0Radius, const Vec2& circle1Pos, double circle1Radius)
 { return circle0Radius < 0 || circle1Radius < 0 ? false : circle0Pos.distance(circle1Pos) <= circle0Radius + circle1Radius; }
 
-bool isInBox3D(const Vec3 &point, const Vec3 boxPos, const Vec3 boxDim)
+bool isInBox3D(const Vec3& point, const Vec3& boxPos, const Vec3& boxDim)
 {
     if (boxDim.x < 0 || boxDim.y < 0 || boxDim.z < 0) return false;
     Vec3 boxEnd = boxPos + boxDim;
     return point.x >= boxPos.x && point.x <= boxEnd.x && point.y >= boxPos.y && point.y <= boxEnd.y && point.z >= boxPos.z && point.z <= boxEnd.z; 
 }
 
-bool isBox3DIntersecting(const Vec3 box0Pos, const Vec3 box0Dim, const Vec3 box1Pos, const Vec3 box1Dim)
+bool isBox3DIntersecting(const Vec3& box0Pos, const Vec3& box0Dim, const Vec3& box1Pos, const Vec3& box1Dim)
 {
     if (box0Dim.x < 0 || box0Dim.y < 0 || box0Dim.z < 0 || box1Dim.x < 0 || box1Dim.y < 0 || box1Dim.z < 0) return false;
     Vec3 box0End = box0Pos + box0Dim;
@@ -75,8 +79,8 @@ bool isBox3DIntersecting(const Vec3 box0Pos, const Vec3 box0Dim, const Vec3 box1
         box0Pos.z <= box1End.z && box0End.z >= box1Pos.z; 
 }
 
-bool isInSphere(const Vec3 &point, const Vec3 spherePos, double sphereRadius)
+bool isInSphere(const Vec3& point, const Vec3& spherePos, double sphereRadius)
 { return sphereRadius < 0 ? false : point.distance(spherePos) <= sphereRadius; }
 
-bool isSphereIntersecting(const Vec3 sphere0Pos, double sphere0Radius, const Vec3 sphere1Pos, double sphere1Radius)
+bool isSphereIntersecting(const Vec3& sphere0Pos, double sphere0Radius, const Vec3& sphere1Pos, double sphere1Radius)
 { return sphere0Radius < 0 || sphere1Radius < 0 ? false : sphere0Pos.distance(sphere1Pos) <= sphere0Radius + sphere1Radius; }
--- src/lib/math/VecMath.h
+++ src/lib/math/VecMath.h
@@ -20,19 +20,19 @@ static const Vec2 VEC2_LEFT     (-1, 0);
 static const Vec2 VEC2_UP       (0, 1);
 static const Vec2 VEC2_DOWN     (0, -1);
 
-Vec2 project3D(Vec3 v);
-Vec2 screenToNormalizedSpace(Vec2 v, int screenWidth, int screenHeight);
-Vec2 normalizedToScreenSpace(Vec2 v, int screenWidth, int screenHeight);
-Vec3 rotate2D(Vec3 v, double angle);
-Vec3 Cross(const Vec3 v1, const Vec3 v2);
+Vec2 project3D(const Vec3& v);
+Vec2 screenToNormalizedSpace(const Vec2& v, int screenWidth, int screenHeight);
+Vec2 normalizedToScreenSpace(const Vec2& v, int screenWidth, int screenHeight);
+Vec3 rotate2D(const Vec3& v, double angle);
+Vec3 Cross(const Vec3& v1, const Vec3& v2);
 bool isInPolygon(const Vec2& point, const Vec2 poly[], int polyLen);
-bool isInBox2D(const Vec2& point, const Vec2 boxPos, const Vec2 boxDim);
-bool isBox2DIntersecting(const Vec2 box0Pos, const Vec2 box0Dim, const Vec2 box1Pos, const Vec2 box1Dim);
-bool isInCircle(const Vec2& point, const Vec2 circlePos, double circleRadius);
-bool isCircleIntersecting(const Vec2 circle0Pos, double circle0Radius, const Vec2 circle1Pos, double circle1Radius);
-bool isInBox3D(const Vec3& point, const Vec3 boxPos, const Vec3 boxDim);
-bool isBox3DIntersecting(const Vec3 box0Pos, const Vec3 box0Dim, const Vec3 box1Pos, const Vec3 box1Dim);
-bool isInSphere(const Vec3& point, const Vec3 spherePos, double sphereRadius);
-bool isSphereIntersecting(const Vec3 sphere0Pos, double sphere0Radius, const Vec3 sphere1Pos, double sphere1Radius);
+bool isInBox2D(const Vec2& point, const Vec2& boxPos, const Vec2& boxDim);
+bool isBox2DIntersecting(const Vec2& box0Pos, const Vec2& box0Dim, const Vec2& box1Pos, const Vec2& box1Dim);
+bool isInCircle(const Vec2& point, const Vec2& circlePos, double circleRadius);
+bool isCircleIntersecting(const Vec2& circle0Pos, double circle0Radius, const Vec2& circle1Pos, double circle1Radius);
+bool isInBox3D(const Vec3& point, const Vec3& boxPos, const Vec3& boxDim);
+bool isBox3DIntersecting(const Vec3& box0Pos, const Vec3& box0Dim, const Vec3& box1Pos, const Vec3& box1Dim);
+bool isInSphere(const Vec3& point, const Vec3& spherePos, double sphereRadius);
+bool isSphereIntersecting(const Vec3& sphere0Pos, double sphere0Radius, const Vec3& sphere1Pos, double sphere1Radius);
 
 #endif
