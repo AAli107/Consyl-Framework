cc8c9e697d6b63a2a7cc0629bdc2bfbd1f223872
Ali107 <Alialawadhi2014bh@gmail.com>
2025-12-27 04:28:37 +0300

    Added isInPolygon

--- src/lib/math/VecMath.cpp
+++ src/lib/math/VecMath.cpp
@@ -11,3 +11,29 @@ Vec2 normalizedToScreenSpace(Vec2 v, int screenWidth, int screenHeight) { return
 Vec3 rotate2D(Vec3 v, double angle) { double c = std::cos(angle); double s = std::sin(angle); return Vec3(c * v.x - s * v.y, s * v.x + c * v.y, v.z); }
 
 Vec3 Cross(const Vec3 v1, const Vec3 v2) { return Vec3( v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x ); }
+
+bool isInPolygon(const Vec2& v, const Vec2 poly[], int polyLen)
+{
+    if (polyLen < 3) return false;
+    
+    int windingN = 0;
+
+    for (int i = 0; i < polyLen; i++)
+    {
+        const Vec2& currentVert = poly[i];
+        const Vec2& nextVert = poly[(i + 1) % polyLen];
+
+        double crossVal = (nextVert.x - currentVert.x) * (v.y - currentVert.y) - (nextVert.y - currentVert.y) * (v.x - currentVert.x);
+
+        if (currentVert.y <= v.y)
+        {
+            if (nextVert.y > v.y && crossVal > 0)
+                windingN++;
+        }
+        else if (nextVert.y <= v.y && crossVal < 0)
+                windingN--;
+    }
+
+    return windingN != 0;
+}
+
--- src/lib/math/VecMath.h
+++ src/lib/math/VecMath.h
@@ -9,5 +9,6 @@ Vec2 screenToNormalizedSpace(Vec2 v, int screenWidth, int screenHeight);
 Vec2 normalizedToScreenSpace(Vec2 v, int screenWidth, int screenHeight);
 Vec3 rotate2D(Vec3 v, double angle);
 Vec3 Cross(const Vec3 v1, const Vec3 v2);
+bool isInPolygon(const Vec2& v, const Vec2 poly[], int polyLen);
 
 #endif
