f23025e2ec8f3fa71693b2ec399065333a21c61c
Ali107 <Alialawadhi2014bh@gmail.com>
2025-12-27 12:43:02 +0300

    Added const values for Vec3 and Vec2

--- src/lib/math/VecMath.h
+++ src/lib/math/VecMath.h
@@ -4,6 +4,22 @@
 #include "Vec2.h"
 #include "Vec3.h"
 
+static const Vec3 VEC3_ZERO     (0, 0, 0);
+static const Vec3 VEC3_ONE      (1, 1, 1);
+static const Vec3 VEC3_RIGHT    (1, 0, 0);
+static const Vec3 VEC3_LEFT     (-1, 0, 0);
+static const Vec3 VEC3_UP       (0, 1, 0);
+static const Vec3 VEC3_DOWN     (0, -1, 0);
+static const Vec3 VEC3_FORWARD  (0, 0, 1);
+static const Vec3 VEC3_BACKWARD (0, 0, -1);
+
+static const Vec2 VEC2_ZERO     (0, 0);
+static const Vec2 VEC2_ONE      (1, 1);
+static const Vec2 VEC2_RIGHT    (1, 0);
+static const Vec2 VEC2_LEFT     (-1, 0);
+static const Vec2 VEC2_UP       (0, 1);
+static const Vec2 VEC2_DOWN     (0, -1);
+
 Vec2 project3D(Vec3 v);
 Vec2 screenToNormalizedSpace(Vec2 v, int screenWidth, int screenHeight);
 Vec2 normalizedToScreenSpace(Vec2 v, int screenWidth, int screenHeight);
