92f952026d31ed646ded2d9eead6d6dba19c2bdf
Ali107 <Alialawadhi2014bh@gmail.com>
2025-12-29 00:17:10 +0300

    Added GameObject header

---
+++ src/lib/engine/GameObject.h
@@ -0,0 +1,25 @@
+#ifndef GAMEOBJECT_H
+
+#include "../math/VecMath.h"
+#include "../math/Transform.h"
+#include <vector>
+#include <concepts>
+#include <memory>
+
+class Component;
+
+class GameObject {
+private:
+    bool enabled = true;
+    std::vector<std::unique_ptr<Component>> components;
+public:
+    Transform transform;
+
+    bool isEnabled() const noexcept { return enabled; }
+    void setEnabled(bool value) noexcept { enabled = value; }
+    template <std::derived_from<Component> T, typename... Args> T& addComponent(Args&&... args);
+    template <std::derived_from<Component> T> T& getComponent();
+    template <std::derived_from<Component> T> bool tryGetComponent(T& component);
+};
+
+#endif
