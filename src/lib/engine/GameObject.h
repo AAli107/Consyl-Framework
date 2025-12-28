ecafb79c3fb047185e098c6361565d3448b7aea9
Ali107 <Alialawadhi2014bh@gmail.com>
2025-12-29 01:53:13 +0300

    Added addComponent Implementation

--- src/lib/engine/GameObject.h
+++ src/lib/engine/GameObject.h
@@ -24,3 +24,13 @@ public:
 };
 
 #endif
+
+template <std::derived_from<Component> T, typename... Args>
+inline T &GameObject::addComponent(Args &&...args)
+{
+    auto comp = std::make_unique<T>(std::forward<Args>(args)...);
+    comp->setParent(*this);
+    T& ref = *comp;
+    components.push_back(std::move(comp));
+    return ref;
+}
