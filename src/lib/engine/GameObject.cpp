27b86eaf89f67d7bbb49fbfe876967a45713a91b
Ali107 <Alialawadhi2014bh@gmail.com>
2025-12-29 13:12:49 +0300

    update, tick, and render functions will now run the game object's components' update, tick, and render functions.

--- src/lib/engine/GameObject.cpp
+++ src/lib/engine/GameObject.cpp
@@ -2,12 +2,21 @@
 
 void GameObject::update(GameLoop &gl)
 {
+    for (auto& c : components)
+        if (c && c->isEnabled())
+            c->update(gl);
 }
 
 void GameObject::tick(GameLoop &gl)
 {
+    for (auto& c : components)
+        if (c && c->isEnabled())
+            c->tick(gl);
 }
 
 void GameObject::render(GameLoop &gl, Gfx &gfx)
 {
+    for (auto& c : components)
+        if (c && c->isEnabled())
+            c->render(gl, gfx);
 }
