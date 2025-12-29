6326127c19ada61dc77b0ee209716c0da52759c8
Ali107 <Alialawadhi2014bh@gmail.com>
2025-12-29 11:43:32 +0300

    Added World.h

---
+++ src/lib/engine/World.h
@@ -0,0 +1,15 @@
+#ifndef WORLD_H
+#define WORLD_H
+
+#include "GameObject.h"
+
+#include <unordered_map>
+#include <string>
+
+struct World
+{
+    std::unordered_map<std::string, std::unique_ptr<GameObject>> gameObjects;
+};
+
+
+#endif
