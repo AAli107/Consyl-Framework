6dae6022d519f68ab2779ee83b0caf0183cee13e
Ali107 <Alialawadhi2014bh@gmail.com>
2026-01-02 13:37:09 +0300

    setConsoleTitle relies on ANSI code on windows

--- src/lib/utils/console.cpp
+++ src/lib/utils/console.cpp
@@ -1,16 +1,6 @@
 #include "console.h"
 
-#ifdef _WIN32
-#include <windows.h>
-#else
 #include <iostream>
-#endif
 
 void setConsoleTitle(const char* title)
-{
-    #ifdef _WIN32
-        SetConsoleTitleA(title);
-    #else
-        std::cout << "\033]0;" << title << "\007";
-    #endif
-}
+{ std::cout << "\033]0;" << title << "\007"; }
