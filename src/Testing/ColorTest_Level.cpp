b5f0e2a4bcb4b4053a6b10ac32cc3a81e8d0f364
Ali107 <Alialawadhi2014bh@gmail.com>
2026-01-02 17:38:34 +0300

    Added test code for color

---
+++ src/Testing/ColorTest_Level.cpp
@@ -0,0 +1,26 @@
+#include "ColorTest_Level.h"
+#include "../lib/engine/Keyboard.h"
+#include "../lib/math/VecMath.h"
+
+#include <cmath>
+
+// This level holds test code for colored Ascii pixels. Main purpose of the test is to test how the colors look.
+// If you want to try and test this, do the following:
+//      - Go to "src/lib/engine/main.cpp" and change startingLevel variable to hold this Level's class name
+//      - static auto startingLevel = SampleLevel();
+//                     Change this -- ^^^^^^^^^^^
+
+
+void ColorTest_Level::start(GameLoop &gl) // Runs when the level starts
+{
+    gl.showStats = true;
+}
+
+void ColorTest_Level::render(GameLoop &gl, Gfx &gfx) // Runs after right after update() for graphics rendering
+{
+    // Having a lot of differently colored Ascii pixels can slow performance 
+    float b = (sin(gl.timeRunning()) + 1) * 2;
+    for (float r = 0; r < 60; r++)
+        for (float g = 0; g < 60; g++)
+            gfx.setPixel(r, g, AsciiPixel('A', Color(r / 60.0f, g / 60.0f, b), Color(0,0,0)));
+}
