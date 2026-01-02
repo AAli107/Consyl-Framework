623d12d0c6fa7fac0c3fd399c5b478ce2dc36198
Ali107 <Alialawadhi2014bh@gmail.com>
2026-01-02 13:34:37 +0300

    Added enableAnsi function

---
+++ src/lib/utils/ansi.cpp
@@ -0,0 +1,33 @@
+#include "ansi.h"
+
+#ifdef _WIN32
+
+#include <windows.h>
+#include <iostream>
+
+int enableANSI()
+{
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (hOut == INVALID_HANDLE_VALUE) {
+        std::cerr << "Failed to get std output handle.\n";
+        return 1;
+    }
+
+    DWORD dwMode = 0;
+    if (!GetConsoleMode(hOut, &dwMode)) {
+        std::cerr << "Failed to get the current console mode.\n";
+        return 1;
+    }
+
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    if (!SetConsoleMode(hOut, dwMode)) {
+        std::cerr << "Failed to set console mode.\n";
+        return 1;
+    }
+
+    return 0;
+}
+
+#else
+int enableANSI() { return 0; } // Should do nothing since non-windows platforms already have ANSI enabled by default
+#endif
---
+++ src/lib/utils/ansi.h
@@ -0,0 +1,7 @@
+#ifndef ANSI_H
+#define ANSI_H
+
+// will run on Windows to enable ANSI
+int enableANSI();
+
+#endif
