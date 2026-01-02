f2ad4b720632d72ca853cd869932b05bb62f443e
Ali107 <Alialawadhi2014bh@gmail.com>
2026-01-02 13:35:15 +0300

    Main function now utilizes enableAnsi function

--- src/lib/engine/main.cpp
+++ src/lib/engine/main.cpp
@@ -1,6 +1,7 @@
 #include "GameLoop.h"
 #include "../../SampleLevel.h"
 #include "../utils/console.h"
+#include "../utils/ansi.h"
 
 static const char* title = "Consyl Game";
 static auto startingLevel = SampleLevel();
@@ -8,6 +9,8 @@ static int tickRate = 60; // per second
 
 int main() 
 {
+    int r = enableANSI();
+    if (r != 0) return r;
     setConsoleTitle(title);
     GameLoop gl = GameLoop(&startingLevel, tickRate);
     gl.run();
