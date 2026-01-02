96ceda505c5e55be3dbb94376acb1506ae51cf9d
Ali107 <Alialawadhi2014bh@gmail.com>
2026-01-02 17:57:39 +0300

    Added setBackgroundColor to console.h

--- src/lib/utils/console.h
+++ src/lib/utils/console.h
@@ -1,10 +1,13 @@
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
+#include "../math/Color.h"
+
 int enableANSI(); // will run on Windows to enable ANSI
 void setConsoleTitle(const char* title);
 void moveCursorTop();
 void hideCursor();
 void showCursor();
+void setBackgroundColor(const Color& c);
 
 #endif
