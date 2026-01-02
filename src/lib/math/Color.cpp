97abd8424056bc7dc6046b36cbf570955b6dde0e
Ali107 <Alialawadhi2014bh@gmail.com>
2026-01-03 02:08:39 +0300

    Fixed scale function + consistency in casting

--- src/lib/math/Color.cpp
+++ src/lib/math/Color.cpp
@@ -1,6 +1,5 @@
 #include "Color.h"
 
-#include <cstdint>
 #include <algorithm>
 #include <cmath>
 #include <sstream>
@@ -11,19 +10,19 @@ Color::Color(unsigned char r, unsigned char g, unsigned char b)
     : r(r), g(g), b(b) {}
 
 Color::Color(int r, int g, int b) : 
-    r(static_cast<std::uint8_t>(std::clamp(r, 0, 255))),
-    g(static_cast<std::uint8_t>(std::clamp(g, 0, 255))),
-    b(static_cast<std::uint8_t>(std::clamp(b, 0, 255))) {}
+    r(static_cast<unsigned char>(std::clamp(r, 0, 255))),
+    g(static_cast<unsigned char>(std::clamp(g, 0, 255))),
+    b(static_cast<unsigned char>(std::clamp(b, 0, 255))) {}
 
 Color::Color(float r, float g, float b) : 
-    r(static_cast<std::uint8_t>(std::clamp(int(std::lround(r * 255.0f)), 0, 255))),
-    g(static_cast<std::uint8_t>(std::clamp(int(std::lround(g * 255.0f)), 0, 255))),
-    b(static_cast<std::uint8_t>(std::clamp(int(std::lround(b * 255.0f)), 0, 255))) {}
+    r(static_cast<unsigned char>(std::clamp(int(std::lround(r * 255.0f)), 0, 255))),
+    g(static_cast<unsigned char>(std::clamp(int(std::lround(g * 255.0f)), 0, 255))),
+    b(static_cast<unsigned char>(std::clamp(int(std::lround(b * 255.0f)), 0, 255))) {}
 
 Color::Color(double r, double g, double b) : 
-    r(static_cast<std::uint8_t>(std::clamp(int(std::lround(r * 255.0)), 0, 255))),
-    g(static_cast<std::uint8_t>(std::clamp(int(std::lround(g * 255.0)), 0, 255))),
-    b(static_cast<std::uint8_t>(std::clamp(int(std::lround(b * 255.0)), 0, 255))) {}
+    r(static_cast<unsigned char>(std::clamp(int(std::lround(r * 255.0)), 0, 255))),
+    g(static_cast<unsigned char>(std::clamp(int(std::lround(g * 255.0)), 0, 255))),
+    b(static_cast<unsigned char>(std::clamp(int(std::lround(b * 255.0)), 0, 255))) {}
 
 Color Color::invert() const
 { return Color(255 - r, 255 - g, 255 - b); }
@@ -32,7 +31,13 @@ Color Color::diff(const Color &c) const
 { return Color(abs(r - c.r), abs(g - c.g), abs(b - c.b)); }
 
 Color Color::scale(const float &v) const
-{ return Color((r / 255.0f) * v, (g / 255.0f) * v, (b / 255.0f) * v); }
+{
+    return Color(
+        static_cast<unsigned char>(std::clamp(int(std::lround(r * v)), 0, 255)),
+        static_cast<unsigned char>(std::clamp(int(std::lround(g * v)), 0, 255)),
+        static_cast<unsigned char>(std::clamp(int(std::lround(b * v)), 0, 255))
+    ); 
+}
 
 Vec3 Color::normalized() const
 { return Vec3(r / 255.0, g / 255.0, b / 255.0); }
