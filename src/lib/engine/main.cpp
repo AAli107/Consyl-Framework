a274766388f73e64322a82c498751fe2bb298a08
Ali107 <Alialawadhi2014bh@gmail.com>
2026-01-07 18:14:19 +0300

    removed unnecessary change

--- src/lib/engine/main.cpp
+++ src/lib/engine/main.cpp
@@ -6,12 +6,11 @@ static const char* title = "Consyl Game";
 static auto startingLevel = SampleLevel();
 static int tickRate = 60; // per second
 
-int main()
+int main() 
 {
     int r = enableANSI();
     if (r != 0) return r;
     setConsoleTitle(title);
-
     GameLoop gl = GameLoop(&startingLevel, tickRate);
     gl.run();
     return 0;
