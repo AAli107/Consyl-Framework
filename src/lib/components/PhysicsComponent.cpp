120b34bb888f78f75fbd8d661c795b9e0de761d8
Ali107 <Alialawadhi2014bh@gmail.com>
2026-01-12 12:47:51 +0300

    Added extra check

--- src/lib/components/PhysicsComponent.cpp
+++ src/lib/components/PhysicsComponent.cpp
@@ -8,8 +8,9 @@ void PhysicsComponent::tick(GameLoop &gl)
         velocity += gForce;
     
     velocity *= 1 / (((doAirborneFriction ? (friction * (1 / mass)) : friction) + 1) >= 1 ? ((doAirborneFriction ? (friction * (1 / mass)) : friction) + 1) : 1);
-
-    parent()->transform.position += velocity;
+    
+    if (velocity != VEC3_ZERO)
+        parent()->transform.position += velocity;
 }
 
 void PhysicsComponent::addVelocity(const Vec3 &velocity, bool ignoreMass)
