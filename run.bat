fa2770f9751531d65757af1b9855c675df543df1
Ali107 <Alialawadhi2014bh@gmail.com>
2026-03-21 05:29:29 +0300

    Reduced cmd clutter when running

--- run.bat
+++ run.bat
@@ -6,6 +6,9 @@ set "OBJECT_FOLDER=obj"
 set "SRC_FOLDER=src"
 set "EXE_OUTPUT_FILE=program.exe"
 
+for /f %%a in ('echo prompt $E^| cmd') do set "ESC=%%a"
+set "CLR=!ESC![2K!ESC![1G"
+
 if exist "%BUILD_FOLDER%" (
     rmdir /s /q "%BUILD_FOLDER%"
 )
@@ -24,10 +27,14 @@ set /a CURRENT=0
 set "OBJ_FILES="
 for /r "%SRC_FOLDER%" %%f in (*.cpp) do (
     set /a CURRENT+=1
-    echo [!CURRENT!/%TOTAL%] Compiling %%~nxf...
+    <nul set /p "=!CLR![!CURRENT!/%TOTAL%] Compiling file: %%~nxf"
+    if !CURRENT! == %TOTAL% (
+        echo(
+    )
     g++ -std=c++20 -O3 -c "%%f" -o "%BUILD_FOLDER%\%OBJECT_FOLDER%\%%~nf.o"
     if errorlevel 1 (
-        echo FAILED: %%~nxf
+        echo Failed to compile "%%~nxf"
+        del /q "%BUILD_FOLDER%\%OBJECT_FOLDER%\*.o"
         exit /b 1
     )
     set "OBJ_FILES=!OBJ_FILES! "%BUILD_FOLDER%\%OBJECT_FOLDER%\%%~nf.o""
@@ -42,6 +49,5 @@ if exist "%BUILD_FOLDER%\%EXE_OUTPUT_FILE%" (
 ) else (
     echo Build failed.
 )
-
 
 del /q "%BUILD_FOLDER%\%OBJECT_FOLDER%\*.o"
